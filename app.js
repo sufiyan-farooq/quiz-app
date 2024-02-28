2ed293bd2c48c2747ea542d81f29369044759c4d
sualeh-farooq <sufiyanfarooq047@gmail.com>
2024-02-28 20:14:55 +0500

    not complete

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+function submitForm() {
+        var userName = "saylani@gmail.com";
+        var userPass = "123";
+        var email = document.getElementById('name').value;
+        var pass = document.getElementById('password').value;
+    
+        if (userName === email && userPass === pass) {
+            location.assign ("main.html")
+        } else {
+         
+            Swal.fire({
+              icon: "error",
+              title: "Incorrect",
+              text: "Please correct email or password!",
+            });
+        }
+    }
+    
+
